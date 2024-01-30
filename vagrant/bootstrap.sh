de0bc0b6494c327e4ab605248d40412af670009d
jmcheon <developer.cheon@gmail.com>
2024-01-30 21:47:53 +0100

    feat: vagrant to set up VM

---
+++ vagrant/bootstrap.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+SERVER_USERNAME="cjung-mo"
+SERVER_USER_PASSWORD="cjung-mo"
+
+function create_user() {
+	# check if the new user already exists.
+	if id -u $SERVER_USERNAME > /dev/null 2>&1; then
+	    echo "User $SERVER_USERNAME already exists."
+	else
+	    sudo useradd -m -s /bin/bash -p $(echo $SERVER_USERNAME| openssl passwd -1 -stdin) $SERVER_USER_PASSWORD
+	    sudo usermod -aG sudo $SERVER_USERNAME
+	    echo "User $SERVER_USERNAME created."
+	fi
+}
+
+function installation() {
+	sudo apt update
+	sudo apt install -y python3 python3-pip
+	pip3 install Django
+}
+
+create_user
+#installation
