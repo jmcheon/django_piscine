29bf93475136276a454dd6178ffea2edaa5e13f3
jmcheon <developer.cheon@gmail.com>
2024-01-30 02:54:07 +0100

    feat: Oob exercises done

--- Oob/ex03/machine.py
+++ Oob/ex03/machine.py
@@ -1,5 +1,7 @@
 import random
 
+from beverages import Cappuccino, Chocolate, Coffee, HotBeverage, Tea
+
 
 class CoffeeMachine:
     def __init__(self):
@@ -11,10 +13,29 @@ class CoffeeMachine:
             super().__init__(price, name)
 
     class BrokenMachineException(Exception):
-        def __init__(self, message="This coffee machine has to be repaired.")
+        def __init__(self, message="This coffee machine has to be repaired."):
             super().__init__(message)
 
     def repair(self):
+        self.count = 0
+        print("machine has been repaired")
 
-    def serve(self):
 
+    def serve(self, instance):
+        self.count += 1
+        if self.count > 9:
+            raise self.BrokenMachineException()
+        if random.randrange(2) == 0:
+            return instance()
+        return self.EmptyCup()
+
+
+if __name__ == "__main__":
+    bev_dict = {0: HotBeverage, 1: Coffee, 2: Tea, 3: Chocolate, 4: Cappuccino}
+    try:
+        machine = CoffeeMachine()
+        for _ in range(10):
+            print(machine.serve(bev_dict.get(random.randrange(5))))
+    except Exception as e:
+        print(e)
+        machine.repair()
