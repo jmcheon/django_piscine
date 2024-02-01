f5be0de691266055f704e57a2735401443e82a9a
jmcheon <developer.cheon@gmail.com>
2024-02-01 16:54:00 +0100

    wip: Lib ex01

--- modules/03/ex01/my_program.py
+++ modules/03/ex01/my_program.py
@@ -1,7 +1,11 @@
-from path import path
+from path import Path as path
 
 
 def ex01():
+    path = path.mkdir("test")
+    file = path.touch(path/"test.txt")
+    file.write_lines(["test", "lines"])
+
 
 if __name__ == "__main__":
     ex01()
