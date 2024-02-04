e20b7871b0d4c8466cf3928b895b20e926711159
jmcheon <developer.cheon@gmail.com>
2024-02-04 18:48:57 +0100

    fix: https:// url append

--- modules/00/ex00/myawesomescript.sh
+++ modules/00/ex00/myawesomescript.sh
@@ -1,5 +1,15 @@
-#!/bin/bash
+#!/bin/sh
 
-URL="https://"$1
+URL=$1
+
+
+#if [[ $URL != https://* ]]; then
+	#URL="https://"$URL
+#fi
+
+case "$URL" in
+    https://*) ;;
+    *) URL="https://$URL" ;;
+esac
 
 curl -s -D - $URL | grep "location" | cut -d' ' -f2
