b8a54c453fffdd1f6ada43baae29f6a92685a57e
jmcheon <developer.cheon@gmail.com>
2025-02-22 18:04:07 +0100

    chore: update fils

--- modules/04/d04/d04/urls.py
+++ modules/04/d04/d04/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for d05 project.
+URL configuration for d04 project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
--- modules/04/d04/d04/wsgi.py
+++ modules/04/d04/d04/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for d05 project.
+WSGI config for d04 project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "d05.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "d04.settings")
 
 application = get_wsgi_application()
