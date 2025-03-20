7cce1b9879baa6bc8b003d73cab6f44b9cd108d0
jungmoo cheon <developer.cheon@gmail.com>
2025-03-20 17:01:55 +0100

    update module 04

--- modules/04/d04/ex01/urls.py
+++ modules/04/d04/ex01/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("", views.django_page, name="home"),
     path("django/", views.django_page, name="django_page"),
     path("display/", views.display_page, name="display_page"),
     path("templates/", views.templates_page, name="templates_page"),
--- modules/04/d04/ex01/views.py
+++ modules/04/d04/ex01/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import render
 
 
-# Create your views here.
+def home(request):
+    return render(request, "ex01/index.html")
+
+
 def django_page(request):
-    return render(request, "ex01/django.html", {"title": "Ex01: Django, framework web."})
+    return render(
+        request, "ex01/django.html", {"title": "Ex01: Django, framework web."}
+    )
 
 
 def display_page(request):
     return render(
-        request, "ex01/display.html", {"title": "Ex01: Display process of a static page."}
+        request,
+        "ex01/display.html",
+        {"title": "Ex01: Display process of a static page."},
     )
 
 
